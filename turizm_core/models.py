27073c63ad9ccdca2260e67fe06964820f2149e0
r10k5 <r10k5.job@gmail.com>
2025-01-07 19:22:59 +0300

    добавлены последние таблицы и произведены миграции

--- turizm_core/models.py
+++ turizm_core/models.py
@@ -35,10 +35,10 @@ class Otel(models.Model):
 
 class DannieAutorizatsii(models.Model):
     role = models.ForeignKey(Role, on_delete=models.PROTECT)
-    nomer_telephona =  models.CharField(max_length=15)
+    nomer_telephona =  models.CharField(max_length=15, unique=True)
     rabochiy_nomer_telephona =  models.CharField(max_length=15, null=True)
     rabochiy_emale = models.CharField(max_length=255, null=True) 
-    emale = models.CharField(max_length=255)
+    emale = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
 
 class Turoperator(models.Model):
@@ -52,3 +52,25 @@ class Polzovatel(models.Model):
     pasport = models.ForeignKey(Pasport, null=True, on_delete=models.SET_NULL)
     zagranpasport = models.ForeignKey(Zagranpasport, null=True, on_delete=models.SET_NULL)
     data_registatsii = models.DateField(auto_now_add=True)
+
+class Putevka(models.Model):
+    turoperator = models.ForeignKey(Turoperator, on_delete=models.CASCADE)
+    otel = models.ForeignKey(Otel, on_delete=models.CASCADE)
+    data_vremya_otpravlenia = models.DateTimeField()
+    data_vremya_vozvrashenia = models.DateTimeField()
+    stoimost = models.IntegerField()
+    data_vremya_zaselenia = models.DateTimeField()
+    data_vremya_viselenia = models.DateTimeField()
+
+class Zakaz(models.Model):
+    putevka = models.ForeignKey(Putevka, on_delete=models.CASCADE)
+    data_sozdania = models.DateField(auto_now_add=True)
+    status = models.IntegerField()
+    dop_info = models.TextField()
+
+class ZakazPolzovatel(models.Model):
+    zakaz = models.ForeignKey(Zakaz, on_delete=models.CASCADE)
+    polzovatel = models.ForeignKey(Polzovatel, on_delete=models.CASCADE)
+    scan_dogovora = models.TextField()
+    nomer_bileta_tuda = models.CharField(max_length=100)
+    nomer_bileta_obratno = models.CharField(max_length=100)
