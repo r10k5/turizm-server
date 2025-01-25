df4f42166a03c0f22bf69f66b1c2528534e05be2
r10k5 <r10k5.job@gmail.com>
2025-01-25 19:48:25 +0300

    отображать туроператору путёвки, убрать поле туроператора из редактирования путёвок и создания, убрать кнопку удалить для туроператора

--- turizm_core/routes/putevka.py
+++ turizm_core/routes/putevka.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import CreateView, DeleteView, UpdateView
@@ -11,11 +12,25 @@ class PutevkaCreateView(CreateView):
     template_name = "putevka/create_view.html"
     success_url = "/putevki"
 
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        if self.request.user.role.id == 'tour_operator':
+            form.fields['turoperator'].initial = self.request.user.turoperator_set.first()
+            form.fields['turoperator'].widget = forms.HiddenInput()
+        return form
+
 class PutevkaUpdateView(UpdateView):
     form_class = PutevkaForm
     model = Putevka
     template_name = "putevka/update_view.html"
     success_url = "/putevki"
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        if self.request.user.role.id == 'tour_operator':
+            form.fields['turoperator'].initial = self.request.user.turoperator_set.first()
+            form.fields['turoperator'].widget = forms.HiddenInput()
+        return form
 
 class PutevkaDeleteView(DeleteView):
     model = Putevka
@@ -23,7 +38,10 @@ class PutevkaDeleteView(DeleteView):
 
 class PutevkaView(View):
     def get(self, request, *args, **kwargs):
-        putevki = Putevka.objects.all()
+        if request.user.role.id == 'tour_operator':
+            putevki = Putevka.objects.filter(turoperator=request.user.turoperator_set.first())
+        else:
+            putevki = Putevka.objects.all()
 
         return render(
             request,
